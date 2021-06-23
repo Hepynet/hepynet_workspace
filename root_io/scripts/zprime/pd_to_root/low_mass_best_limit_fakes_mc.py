7794769887757f00d7597c0abe8317f0fdaa2e69
Zhe Yang <zhe.yang@cern.ch>
2021-06-23 21:33:14 +0200

    Update configs and add fakes

---
+++ root_io/scripts/zprime/pd_to_root/low_mass_best_limit_fakes_mc.py
@@ -0,0 +1,42 @@
+import array
+import pathlib
+
+import numpy as np
+import pandas as pd
+import ROOT
+
+df_dir = pathlib.Path("/data/zprime/data_frames_fit/21-0120-sys-best-limit/low_mass")
+ntup_dir = pathlib.Path("/data/zprime/ntuples_fit/21-0120-sys-best-limit/low_mass")
+ntup_dir.mkdir(parents=True, exist_ok=True)
+
+samples = [
+    "bkg_fakes_diboson",
+    "bkg_fakes_ttbar",
+    "bkg_fakes_zll",
+]
+branch_list = ["mz1", "mz2", "dnn_out_sig", "weight"]
+ntup_name = "ntup"
+
+var = "tree_NOMINAL"
+print(f"Variation: {var}")
+ntup_dir_var = ntup_dir / var
+ntup_dir_var.mkdir(parents=True, exist_ok=True)
+for sample in samples:
+    print(f"Generating fit ntuples for: {sample}")
+    # read dataframe
+    df_path = df_dir / var / f"{sample}.feather"
+    df: pd.DataFrame = pd.read_feather(df_path)
+    # prepare ntuple file
+    ntup_path = ntup_dir_var / f"{sample}.root"
+    root_file = ROOT.TFile(ntup_path.as_posix(), "RECREATE")
+    root_file.cd()
+    # dump
+    dump_features = branch_list[:]
+    ntup = ROOT.TNtuple(ntup_name, ntup_name, ":".join(dump_features))
+    fit_array = df[dump_features].to_numpy()
+    for row in fit_array:
+        fill_values = list(row)
+        ntup.Fill(array.array("f", fill_values))
+    root_file.cd()
+    ntup.Write()
+    root_file.Close()
