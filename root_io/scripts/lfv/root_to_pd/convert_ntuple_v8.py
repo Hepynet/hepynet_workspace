ca152c4c37c9f152feaf1d942b6157048219acea
Zhe Yang <starprecursor@gmail.com>
2021-06-30 02:06:34 +0200

    Update for LFV new samples

--- root_io/scripts/lfv/root_to_pd/convert_ntuple_v8.py
+++ root_io/scripts/lfv/root_to_pd/convert_ntuple_v8.py
@@ -1,5 +1,5 @@
 import pathlib
-
+import numpy as np
 import pandas as pd
 import uproot
 
@@ -10,6 +10,8 @@ ntup_dir = pathlib.Path(f"/data/lfv/ntuples/v8")
 df_dir = pathlib.Path(f"/data/lfv/data_frames/v8")
 df_dir.mkdir(parents=True, exist_ok=True)
 tree_name = "lfvTree"
+
+b_veto_features = ["NbjetsDL1r85", "Weight_bTagSF_DL1r_85"]
 
 # convert to pandas DataFrame
 def process_sample(
@@ -27,18 +29,21 @@ def process_sample(
     sample_dfs = list()
     for chunk_pd in uproot.iterate(
         f"{sample_path}:{tree_name}",
-        feature_list[:],
+        feature_list[:]+b_veto_features,
         cut=cut,
         library="pd",
         step_size="200 MB",
     ):
+        chunk_pd: pd.DataFrame
         # use lower case for features' names
-        chunk_pd.columns = feature_names[:]
+        chunk_pd.columns = feature_names[:]+b_veto_features
         # physic para for pNN
         if ("bkg" in sample_name) or ("data" in sample_name):
             chunk_pd = chunk_pd.assign(m_truth=chunk_pd[m_truth_name])
         elif "sig" in sample_name:
             chunk_pd = chunk_pd.assign(m_truth=m_truth_dict[sample_name])
+        # scale weight for b_beto
+        chunk_pd["weight"] = chunk_pd["weight"].multiply(chunk_pd["Weight_bTagSF_DL1r_85"])
         # convert float64 to float32
         f64_cols = chunk_pd.select_dtypes(include="float64").columns
         chunk_pd[f64_cols] = chunk_pd[f64_cols].astype("float32")
@@ -53,39 +58,42 @@ def process_sample(
 
 
 # prepare emu
-print("## Processing channel: emu")
-feature_list_in = feature_list_in_emu
-feature_list_out = feature_list_out_emu
-df_emu_list = list()
-for bkg_name, bkg_file in bkg_dict.items():
-    root_path = ntup_dir / "background" / f"{bkg_file}.root"
-    df_emu_list += process_sample(
-        feature_list=feature_list_in,
-        feature_names=feature_list_out,
-        sample_name=bkg_name,
-        sample_path=root_path,
-        is_sig=False,
-        is_mc=True,
-        m_truth_name="ll_m",
-        tree_name=tree_name,
-        cut="emuChannel == 1",
-    )
-for sig_name, sig_file in sig_dict_emu.items():
-    root_path = ntup_dir / "signal" / f"{sig_file}.root"
-    df_emu_list += process_sample(
-        feature_list=feature_list_in,
-        feature_names=feature_list_out,
-        sample_name=sig_name,
-        sample_path=root_path,
-        is_sig=True,
-        is_mc=True,
-        m_truth_name="ll_m",
-        tree_name=tree_name,
-        cut="emuChannel == 1",
-    )
-df_emu: pd.DataFrame = pd.concat(df_emu_list, ignore_index=True)
-del df_emu_list
-save_path = df_dir / "lfv_emu.feather"
-print(f"## Saving to {save_path}")
-df_emu.to_feather(save_path)
-del df_emu
+
+for channel in ["emu", "etau", "mutau"]:
+
+    print(f"## Processing channel: {channel}")
+    feature_list_in = feature_list_in_dict[channel]
+    feature_list_out = feature_list_out_dict[channel]
+    df_list = list()
+    for bkg_name, bkg_file in bkg_dict.items():
+        root_path = ntup_dir / "background" / f"{bkg_file}.root"
+        df_list += process_sample(
+            feature_list=feature_list_in,
+            feature_names=feature_list_out,
+            sample_name=bkg_name,
+            sample_path=root_path,
+            is_sig=False,
+            is_mc=True,
+            m_truth_name="ll_m",
+            tree_name=tree_name,
+            cut=f"({channel}Channel == 1) & (NbjetsDL1r85 == 0)",
+        )
+    for sig_name, sig_file in sig_dict.items():
+        root_path = ntup_dir / "signal" / f"{sig_file}.root"
+        df_list += process_sample(
+            feature_list=feature_list_in,
+            feature_names=feature_list_out,
+            sample_name=sig_name,
+            sample_path=root_path,
+            is_sig=True,
+            is_mc=True,
+            m_truth_name="ll_m",
+            tree_name=tree_name,
+            cut=f"({channel}Channel == 1) & (NbjetsDL1r85 == 0)",
+        )
+    df: pd.DataFrame = pd.concat(df_list, ignore_index=True)
+    del df_list
+    save_path = df_dir / f"lfv_{channel}.feather"
+    print(f"## Saving to {save_path}")
+    df.to_feather(save_path)
+    del df
--- root_io/scripts/lfv/root_to_pd/lfv_const.py
+++ root_io/scripts/lfv/root_to_pd/lfv_const.py
@@ -1,3 +1,5 @@
+# feature names of input root files
+
 feature_list_in_emu = [
     "Electron_Pt",
     "Electron_Eta",
@@ -11,6 +13,7 @@ feature_list_in_emu = [
     "Muon_Charge",
     "MET_Et",
     "MET_Phi",
+    "Transverse_Mass",
     "DeltaPhi_ll",
     "DeltaPhi_DilMET",
     "RecoDilRapidity",
@@ -19,6 +22,62 @@ feature_list_in_emu = [
     "RecoDilPt",
     "RecoWeight",
 ]
+
+feature_list_in_etau = [
+    "Electron_Pt",
+    "Electron_Eta",
+    "Electron_Phi",
+    "Electron_E",
+    "Electron_Charge",
+    "Tau_Pt",
+    "Tau_Eta",
+    "Tau_Phi",
+    "Tau_E",
+    "Tau_Charge",
+    "Tau_nTrack",
+    "MET_Et",
+    "MET_Phi",
+    "Transverse_Mass",
+    "DeltaPhi_ll",
+    "DeltaPhi_DilMET",
+    "RecoDilRapidity",
+    "RecoDilPhi",
+    "RecoDilMass",
+    "RecoDilPt",
+    "RecoWeight",
+]
+
+feature_list_in_mutau = [
+    "Muon_Pt",
+    "Muon_Eta",
+    "Muon_Phi",
+    "Muon_E",
+    "Muon_Charge",
+    "Tau_Pt",
+    "Tau_Eta",
+    "Tau_Phi",
+    "Tau_E",
+    "Tau_Charge",
+    "Tau_nTrack",
+    "MET_Et",
+    "MET_Phi",
+    "Transverse_Mass",
+    "DeltaPhi_ll",
+    "DeltaPhi_DilMET",
+    "RecoDilRapidity",
+    "RecoDilPhi",
+    "RecoDilMass",
+    "RecoDilPt",
+    "RecoWeight",
+]
+
+feature_list_in_dict = {
+    "emu": feature_list_in_emu,
+    "etau": feature_list_in_etau,
+    "mutau": feature_list_in_mutau,
+}
+
+# feature names for training
 
 feature_list_out_emu = [
     "ele_pt",
@@ -33,6 +92,7 @@ feature_list_out_emu = [
     "mu_c",
     "met_et",
     "met_phi",
+    "m_t",
     "ll_dphi",
     "ll_met_dphi",
     "ll_y",
@@ -42,6 +102,62 @@ feature_list_out_emu = [
     "weight",
 ]
 
+feature_list_out_etau = [
+    "ele_pt",
+    "ele_eta",
+    "ele_phi",
+    "ele_e",
+    "ele_c",
+    "tau_pt",
+    "tau_eta",
+    "tau_phi",
+    "tau_e",
+    "tau_c",
+    "tau_ntracks",
+    "met_et",
+    "met_phi",
+    "m_t",
+    "ll_dphi",
+    "ll_met_dphi",
+    "ll_y",
+    "ll_phi",
+    "ll_m",
+    "ll_pt",
+    "weight",
+]
+
+feature_list_out_mutau = [
+    "mu_pt",
+    "mu_eta",
+    "mu_phi",
+    "mu_e",
+    "mu_c",
+    "tau_pt",
+    "tau_eta",
+    "tau_phi",
+    "tau_e",
+    "tau_c",
+    "tau_ntracks",
+    "met_et",
+    "met_phi",
+    "m_t",
+    "ll_dphi",
+    "ll_met_dphi",
+    "ll_y",
+    "ll_phi",
+    "ll_m",
+    "ll_pt",
+    "weight",
+]
+
+feature_list_out_dict = {
+    "emu": feature_list_out_emu,
+    "etau": feature_list_out_etau,
+    "mutau": feature_list_out_mutau,
+}
+
+# file name maps
+
 bkg_dict = {
     "bkg_diboson": "output_nominal_Diboson_nomWeight",
     "bkg_top": "output_nominal_TopQuark_nomWeight",
@@ -49,94 +165,94 @@ bkg_dict = {
     "bkg_zll": "output_nominal_Ztoll_nomWeight",
 }
 
-sig_dict_emu = {
+sig_dict = {
     # QBH n1
-    "sig_qbh_n1_200": "output_nominal_QBH_n1_emu200_nomWeight",
-    "sig_qbh_n1_250": "output_nominal_QBH_n1_emu250_nomWeight",
-    "sig_qbh_n1_300": "output_nominal_QBH_n1_emu300_nomWeight",
-    "sig_qbh_n1_350": "output_nominal_QBH_n1_emu350_nomWeight",
-    "sig_qbh_n1_400": "output_nominal_QBH_n1_emu400_nomWeight",
-    "sig_qbh_n1_450": "output_nominal_QBH_n1_emu450_nomWeight",
-    "sig_qbh_n1_500": "output_nominal_QBH_n1_emu500_nomWeight",
-    "sig_qbh_n1_550": "output_nominal_QBH_n1_emu550_nomWeight",
-    "sig_qbh_n1_600": "output_nominal_QBH_n1_emu600_nomWeight",
-    "sig_qbh_n1_650": "output_nominal_QBH_n1_emu650_nomWeight",
-    "sig_qbh_n1_700": "output_nominal_QBH_n1_emu700_nomWeight",
+    "sig_qbh_n1_2000": "output_nominal_QBH_n1_*200_nomWeight",
+    "sig_qbh_n1_2500": "output_nominal_QBH_n1_*250_nomWeight",
+    "sig_qbh_n1_3000": "output_nominal_QBH_n1_*300_nomWeight",
+    "sig_qbh_n1_3500": "output_nominal_QBH_n1_*350_nomWeight",
+    "sig_qbh_n1_4000": "output_nominal_QBH_n1_*400_nomWeight",
+    "sig_qbh_n1_4500": "output_nominal_QBH_n1_*450_nomWeight",
+    "sig_qbh_n1_5000": "output_nominal_QBH_n1_*500_nomWeight",
+    "sig_qbh_n1_5500": "output_nominal_QBH_n1_*550_nomWeight",
+    "sig_qbh_n1_6000": "output_nominal_QBH_n1_*600_nomWeight",
+    "sig_qbh_n1_6500": "output_nominal_QBH_n1_*650_nomWeight",
+    "sig_qbh_n1_7000": "output_nominal_QBH_n1_*700_nomWeight",
     # QBH n2
-    "sig_qbh_n2_400": "output_nominal_QBH_n6_emu400_nomWeight",
-    "sig_qbh_n2_450": "output_nominal_QBH_n6_emu450_nomWeight",
-    "sig_qbh_n2_500": "output_nominal_QBH_n6_emu500_nomWeight",
-    "sig_qbh_n2_550": "output_nominal_QBH_n6_emu550_nomWeight",
-    "sig_qbh_n2_600": "output_nominal_QBH_n6_emu600_nomWeight",
-    "sig_qbh_n2_650": "output_nominal_QBH_n6_emu650_nomWeight",
-    "sig_qbh_n2_700": "output_nominal_QBH_n6_emu700_nomWeight",
-    "sig_qbh_n2_750": "output_nominal_QBH_n6_emu750_nomWeight",
-    "sig_qbh_n2_800": "output_nominal_QBH_n6_emu800_nomWeight",
-    "sig_qbh_n2_850": "output_nominal_QBH_n6_emu850_nomWeight",
-    "sig_qbh_n2_900": "output_nominal_QBH_n6_emu900_nomWeight",
+    "sig_qbh_n2_4000": "output_nominal_QBH_n6_*400_nomWeight",
+    "sig_qbh_n2_4500": "output_nominal_QBH_n6_*450_nomWeight",
+    "sig_qbh_n2_5000": "output_nominal_QBH_n6_*500_nomWeight",
+    "sig_qbh_n2_5500": "output_nominal_QBH_n6_*550_nomWeight",
+    "sig_qbh_n2_6000": "output_nominal_QBH_n6_*600_nomWeight",
+    "sig_qbh_n2_6500": "output_nominal_QBH_n6_*650_nomWeight",
+    "sig_qbh_n2_7000": "output_nominal_QBH_n6_*700_nomWeight",
+    "sig_qbh_n2_7500": "output_nominal_QBH_n6_*750_nomWeight",
+    "sig_qbh_n2_8000": "output_nominal_QBH_n6_*800_nomWeight",
+    "sig_qbh_n2_8500": "output_nominal_QBH_n6_*850_nomWeight",
+    "sig_qbh_n2_9000": "output_nominal_QBH_n6_*900_nomWeight",
     # SVT
-    "sig_svt_500": "output_nominal_SVT_emu500_nomWeight",
-    "sig_svt_700": "output_nominal_SVT_emu700_nomWeight",
-    "sig_svt_1000": "output_nominal_SVT_emu1000_nomWeight",
-    "sig_svt_1500": "output_nominal_SVT_emu1500_nomWeight",
-    "sig_svt_2000": "output_nominal_SVT_emu2000_nomWeight",
-    "sig_svt_2500": "output_nominal_SVT_emu2500_nomWeight",
-    "sig_svt_3000": "output_nominal_SVT_emu3000_nomWeight",
-    "sig_svt_3500": "output_nominal_SVT_emu3500_nomWeight",
-    "sig_svt_4000": "output_nominal_SVT_emu4000_nomWeight",
-    "sig_svt_4500": "output_nominal_SVT_emu4500_nomWeight",
-    "sig_svt_5000": "output_nominal_SVT_emu5000_nomWeight",
-    "sig_svt_5500": "output_nominal_SVT_emu5500_nomWeight",
-    "sig_svt_6000": "output_nominal_SVT_emu6000_nomWeight",
-    "sig_svt_6500": "output_nominal_SVT_emu6500_nomWeight",
-    "sig_svt_7000": "output_nominal_SVT_emu7000_nomWeight",
-    "sig_svt_7500": "output_nominal_SVT_emu7500_nomWeight",
-    "sig_svt_8000": "output_nominal_SVT_emu8000_nomWeight",
+    "sig_svt_500": "output_nominal_SVT_*500_nomWeight",
+    "sig_svt_700": "output_nominal_SVT_*700_nomWeight",
+    "sig_svt_1000": "output_nominal_SVT_*1000_nomWeight",
+    "sig_svt_1500": "output_nominal_SVT_*1500_nomWeight",
+    "sig_svt_2000": "output_nominal_SVT_*2000_nomWeight",
+    "sig_svt_2500": "output_nominal_SVT_*2500_nomWeight",
+    "sig_svt_3000": "output_nominal_SVT_*3000_nomWeight",
+    "sig_svt_3500": "output_nominal_SVT_*3500_nomWeight",
+    "sig_svt_4000": "output_nominal_SVT_*4000_nomWeight",
+    "sig_svt_4500": "output_nominal_SVT_*4500_nomWeight",
+    "sig_svt_5000": "output_nominal_SVT_*5000_nomWeight",
+    "sig_svt_5500": "output_nominal_SVT_*5500_nomWeight",
+    "sig_svt_6000": "output_nominal_SVT_*6000_nomWeight",
+    "sig_svt_6500": "output_nominal_SVT_*6500_nomWeight",
+    "sig_svt_7000": "output_nominal_SVT_*7000_nomWeight",
+    "sig_svt_7500": "output_nominal_SVT_*7500_nomWeight",
+    "sig_svt_8000": "output_nominal_SVT_*8000_nomWeight",
     # Zprime
-    "sig_zp_500": "output_nominal_Zprime_emu500_nomWeight",
-    "sig_zp_700": "output_nominal_Zprime_emu700_nomWeight",
-    "sig_zp_1000": "output_nominal_Zprime_emu1000_nomWeight",
-    "sig_zp_1500": "output_nominal_Zprime_emu1500_nomWeight",
-    "sig_zp_2000": "output_nominal_Zprime_emu2000_nomWeight",
-    "sig_zp_2500": "output_nominal_Zprime_emu2500_nomWeight",
-    "sig_zp_3000": "output_nominal_Zprime_emu3000_nomWeight",
-    "sig_zp_3500": "output_nominal_Zprime_emu3500_nomWeight",
-    "sig_zp_4000": "output_nominal_Zprime_emu4000_nomWeight",
-    "sig_zp_4500": "output_nominal_Zprime_emu4500_nomWeight",
-    "sig_zp_5000": "output_nominal_Zprime_emu5000_nomWeight",
-    "sig_zp_5500": "output_nominal_Zprime_emu5500_nomWeight",
-    "sig_zp_6000": "output_nominal_Zprime_emu6000_nomWeight",
-    "sig_zp_6500": "output_nominal_Zprime_emu6500_nomWeight",
-    "sig_zp_7000": "output_nominal_Zprime_emu7000_nomWeight",
-    "sig_zp_7500": "output_nominal_Zprime_emu7500_nomWeight",
-    "sig_zp_8000": "output_nominal_Zprime_emu8000_nomWeight",
+    "sig_zp_500": "output_nominal_Zprime_*500_nomWeight",
+    "sig_zp_700": "output_nominal_Zprime_*700_nomWeight",
+    "sig_zp_1000": "output_nominal_Zprime_*1000_nomWeight",
+    "sig_zp_1500": "output_nominal_Zprime_*1500_nomWeight",
+    "sig_zp_2000": "output_nominal_Zprime_*2000_nomWeight",
+    "sig_zp_2500": "output_nominal_Zprime_*2500_nomWeight",
+    "sig_zp_3000": "output_nominal_Zprime_*3000_nomWeight",
+    "sig_zp_3500": "output_nominal_Zprime_*3500_nomWeight",
+    "sig_zp_4000": "output_nominal_Zprime_*4000_nomWeight",
+    "sig_zp_4500": "output_nominal_Zprime_*4500_nomWeight",
+    "sig_zp_5000": "output_nominal_Zprime_*5000_nomWeight",
+    "sig_zp_5500": "output_nominal_Zprime_*5500_nomWeight",
+    "sig_zp_6000": "output_nominal_Zprime_*6000_nomWeight",
+    "sig_zp_6500": "output_nominal_Zprime_*6500_nomWeight",
+    "sig_zp_7000": "output_nominal_Zprime_*7000_nomWeight",
+    "sig_zp_7500": "output_nominal_Zprime_*7500_nomWeight",
+    "sig_zp_8000": "output_nominal_Zprime_*8000_nomWeight",
 }
 
 m_truth_dict = {
     # QBH n1
-    "sig_qbh_n1_200": 200,
-    "sig_qbh_n1_250": 250,
-    "sig_qbh_n1_300": 300,
-    "sig_qbh_n1_350": 350,
-    "sig_qbh_n1_400": 400,
-    "sig_qbh_n1_450": 450,
-    "sig_qbh_n1_500": 500,
-    "sig_qbh_n1_550": 550,
-    "sig_qbh_n1_600": 600,
-    "sig_qbh_n1_650": 650,
-    "sig_qbh_n1_700": 700,
+    "sig_qbh_n1_2000": 2000,
+    "sig_qbh_n1_2500": 2500,
+    "sig_qbh_n1_3000": 3000,
+    "sig_qbh_n1_3500": 3500,
+    "sig_qbh_n1_4000": 4000,
+    "sig_qbh_n1_4500": 4500,
+    "sig_qbh_n1_5000": 5000,
+    "sig_qbh_n1_5500": 5500,
+    "sig_qbh_n1_6000": 6000,
+    "sig_qbh_n1_6500": 6500,
+    "sig_qbh_n1_7000": 7000,
     # QBH n2
-    "sig_qbh_n2_400": 400,
-    "sig_qbh_n2_450": 450,
-    "sig_qbh_n2_500": 500,
-    "sig_qbh_n2_550": 550,
-    "sig_qbh_n2_600": 600,
-    "sig_qbh_n2_650": 650,
-    "sig_qbh_n2_700": 700,
-    "sig_qbh_n2_750": 750,
-    "sig_qbh_n2_800": 800,
-    "sig_qbh_n2_850": 850,
-    "sig_qbh_n2_900": 900,
+    "sig_qbh_n2_4000": 4000,
+    "sig_qbh_n2_4500": 4500,
+    "sig_qbh_n2_5000": 5000,
+    "sig_qbh_n2_5500": 5500,
+    "sig_qbh_n2_6000": 6000,
+    "sig_qbh_n2_6500": 6500,
+    "sig_qbh_n2_7000": 7000,
+    "sig_qbh_n2_7500": 7500,
+    "sig_qbh_n2_8000": 8000,
+    "sig_qbh_n2_8500": 8500,
+    "sig_qbh_n2_9000": 9000,
     # SVT
     "sig_svt_500": 500,
     "sig_svt_700": 700,
@@ -174,173 +290,3 @@ m_truth_dict = {
     "sig_zp_7500": 7500,
     "sig_zp_8000": 8000,
 }
-
-
-# "output_nominal_QBH_n1_emu200_nomWeight"
-# "output_nominal_QBH_n1_emu250_nomWeight"
-# "output_nominal_QBH_n1_emu300_nomWeight"
-# "output_nominal_QBH_n1_emu350_nomWeight"
-# "output_nominal_QBH_n1_emu400_nomWeight"
-# "output_nominal_QBH_n1_emu450_nomWeight"
-# "output_nominal_QBH_n1_emu500_nomWeight"
-# "output_nominal_QBH_n1_emu550_nomWeight"
-# "output_nominal_QBH_n1_emu600_nomWeight"
-# "output_nominal_QBH_n1_emu650_nomWeight"
-# "output_nominal_QBH_n1_emu700_nomWeight"
-# "output_nominal_QBH_n1_etau200_nomWeight"
-# "output_nominal_QBH_n1_etau250_nomWeight"
-# "output_nominal_QBH_n1_etau300_nomWeight"
-# "output_nominal_QBH_n1_etau350_nomWeight"
-# "output_nominal_QBH_n1_etau400_nomWeight"
-# "output_nominal_QBH_n1_etau450_nomWeight"
-# "output_nominal_QBH_n1_etau500_nomWeight"
-# "output_nominal_QBH_n1_etau550_nomWeight"
-# "output_nominal_QBH_n1_etau600_nomWeight"
-# "output_nominal_QBH_n1_etau650_nomWeight"
-# "output_nominal_QBH_n1_etau700_nomWeight"
-# "output_nominal_QBH_n1_mutau200_nomWeight"
-# "output_nominal_QBH_n1_mutau250_nomWeight"
-# "output_nominal_QBH_n1_mutau300_nomWeight"
-# "output_nominal_QBH_n1_mutau350_nomWeight"
-# "output_nominal_QBH_n1_mutau400_nomWeight"
-# "output_nominal_QBH_n1_mutau450_nomWeight"
-# "output_nominal_QBH_n1_mutau500_nomWeight"
-# "output_nominal_QBH_n1_mutau550_nomWeight"
-# "output_nominal_QBH_n1_mutau600_nomWeight"
-# "output_nominal_QBH_n1_mutau650_nomWeight"
-# "output_nominal_QBH_n1_mutau700_nomWeight"
-# "output_nominal_QBH_n6_emu400_nomWeight"
-# "output_nominal_QBH_n6_emu450_nomWeight"
-# "output_nominal_QBH_n6_emu500_nomWeight"
-# "output_nominal_QBH_n6_emu550_nomWeight"
-# "output_nominal_QBH_n6_emu600_nomWeight"
-# "output_nominal_QBH_n6_emu650_nomWeight"
-# "output_nominal_QBH_n6_emu700_nomWeight"
-# "output_nominal_QBH_n6_emu750_nomWeight"
-# "output_nominal_QBH_n6_emu800_nomWeight"
-# "output_nominal_QBH_n6_emu850_nomWeight"
-# "output_nominal_QBH_n6_emu900_nomWeight"
-# "output_nominal_QBH_n6_etau400_nomWeight"
-# "output_nominal_QBH_n6_etau450_nomWeight"
-# "output_nominal_QBH_n6_etau500_nomWeight"
-# "output_nominal_QBH_n6_etau550_nomWeight"
-# "output_nominal_QBH_n6_etau600_nomWeight"
-# "output_nominal_QBH_n6_etau650_nomWeight"
-# "output_nominal_QBH_n6_etau700_nomWeight"
-# "output_nominal_QBH_n6_etau750_nomWeight"
-# "output_nominal_QBH_n6_etau800_nomWeight"
-# "output_nominal_QBH_n6_etau850_nomWeight"
-# "output_nominal_QBH_n6_etau900_nomWeight"
-# "output_nominal_QBH_n6_mutau400_nomWeight"
-# "output_nominal_QBH_n6_mutau450_nomWeight"
-# "output_nominal_QBH_n6_mutau500_nomWeight"
-# "output_nominal_QBH_n6_mutau550_nomWeight"
-# "output_nominal_QBH_n6_mutau600_nomWeight"
-# "output_nominal_QBH_n6_mutau650_nomWeight"
-# "output_nominal_QBH_n6_mutau700_nomWeight"
-# "output_nominal_QBH_n6_mutau750_nomWeight"
-# "output_nominal_QBH_n6_mutau800_nomWeight"
-# "output_nominal_QBH_n6_mutau850_nomWeight"
-# "output_nominal_QBH_n6_mutau900_nomWeight"
-# "output_nominal_SVT_emu1000_nomWeight"
-# "output_nominal_SVT_emu1500_nomWeight"
-# "output_nominal_SVT_emu2000_nomWeight"
-# "output_nominal_SVT_emu2500_nomWeight"
-# "output_nominal_SVT_emu3000_nomWeight"
-# "output_nominal_SVT_emu3500_nomWeight"
-# "output_nominal_SVT_emu4000_nomWeight"
-# "output_nominal_SVT_emu4500_nomWeight"
-# "output_nominal_SVT_emu500_nomWeight"
-# "output_nominal_SVT_emu5000_nomWeight"
-# "output_nominal_SVT_emu5500_nomWeight"
-# "output_nominal_SVT_emu6000_nomWeight"
-# "output_nominal_SVT_emu6500_nomWeight"
-# "output_nominal_SVT_emu700_nomWeight"
-# "output_nominal_SVT_emu7000_nomWeight"
-# "output_nominal_SVT_emu7500_nomWeight"
-# "output_nominal_SVT_emu8000_nomWeight"
-# "output_nominal_SVT_etau1000_nomWeight"
-# "output_nominal_SVT_etau1500_nomWeight"
-# "output_nominal_SVT_etau2000_nomWeight"
-# "output_nominal_SVT_etau2500_nomWeight"
-# "output_nominal_SVT_etau3000_nomWeight"
-# "output_nominal_SVT_etau3500_nomWeight"
-# "output_nominal_SVT_etau4000_nomWeight"
-# "output_nominal_SVT_etau4500_nomWeight"
-# "output_nominal_SVT_etau500_nomWeight"
-# "output_nominal_SVT_etau5000_nomWeight"
-# "output_nominal_SVT_etau5500_nomWeight"
-# "output_nominal_SVT_etau6000_nomWeight"
-# "output_nominal_SVT_etau6500_nomWeight"
-# "output_nominal_SVT_etau700_nomWeight"
-# "output_nominal_SVT_etau7000_nomWeight"
-# "output_nominal_SVT_etau7500_nomWeight"
-# "output_nominal_SVT_etau8000_nomWeight"
-# "output_nominal_SVT_mutau1000_nomWeight"
-# "output_nominal_SVT_mutau1500_nomWeight"
-# "output_nominal_SVT_mutau2000_nomWeight"
-# "output_nominal_SVT_mutau2500_nomWeight"
-# "output_nominal_SVT_mutau3000_nomWeight"
-# "output_nominal_SVT_mutau3500_nomWeight"
-# "output_nominal_SVT_mutau4000_nomWeight"
-# "output_nominal_SVT_mutau4500_nomWeight"
-# "output_nominal_SVT_mutau500_nomWeight"
-# "output_nominal_SVT_mutau5000_nomWeight"
-# "output_nominal_SVT_mutau5500_nomWeight"
-# "output_nominal_SVT_mutau6000_nomWeight"
-# "output_nominal_SVT_mutau6500_nomWeight"
-# "output_nominal_SVT_mutau700_nomWeight"
-# "output_nominal_SVT_mutau7000_nomWeight"
-# "output_nominal_SVT_mutau7500_nomWeight"
-# "output_nominal_SVT_mutau8000_nomWeight"
-# "output_nominal_Zprime_emu1000_nomWeight"
-# "output_nominal_Zprime_emu1500_nomWeight"
-# "output_nominal_Zprime_emu2000_nomWeight"
-# "output_nominal_Zprime_emu2500_nomWeight"
-# "output_nominal_Zprime_emu3000_nomWeight"
-# "output_nominal_Zprime_emu3500_nomWeight"
-# "output_nominal_Zprime_emu4000_nomWeight"
-# "output_nominal_Zprime_emu4500_nomWeight"
-# "output_nominal_Zprime_emu500_nomWeight"
-# "output_nominal_Zprime_emu5000_nomWeight"
-# "output_nominal_Zprime_emu5500_nomWeight"
-# "output_nominal_Zprime_emu6000_nomWeight"
-# "output_nominal_Zprime_emu6500_nomWeight"
-# "output_nominal_Zprime_emu700_nomWeight"
-# "output_nominal_Zprime_emu7000_nomWeight"
-# "output_nominal_Zprime_emu7500_nomWeight"
-# "output_nominal_Zprime_emu8000_nomWeight"
-# "output_nominal_Zprime_etau1000_nomWeight"
-# "output_nominal_Zprime_etau1500_nomWeight"
-# "output_nominal_Zprime_etau2000_nomWeight"
-# "output_nominal_Zprime_etau2500_nomWeight"
-# "output_nominal_Zprime_etau3000_nomWeight"
-# "output_nominal_Zprime_etau3500_nomWeight"
-# "output_nominal_Zprime_etau4000_nomWeight"
-# "output_nominal_Zprime_etau4500_nomWeight"
-# "output_nominal_Zprime_etau500_nomWeight"
-# "output_nominal_Zprime_etau5000_nomWeight"
-# "output_nominal_Zprime_etau5500_nomWeight"
-# "output_nominal_Zprime_etau6000_nomWeight"
-# "output_nominal_Zprime_etau6500_nomWeight"
-# "output_nominal_Zprime_etau700_nomWeight"
-# "output_nominal_Zprime_etau7000_nomWeight"
-# "output_nominal_Zprime_etau7500_nomWeight"
-# "output_nominal_Zprime_etau8000_nomWeight"
-# "output_nominal_Zprime_mutau1000_nomWeight"
-# "output_nominal_Zprime_mutau1500_nomWeight"
-# "output_nominal_Zprime_mutau2000_nomWeight"
-# "output_nominal_Zprime_mutau2500_nomWeight"
-# "output_nominal_Zprime_mutau3000_nomWeight"
-# "output_nominal_Zprime_mutau3500_nomWeight"
-# "output_nominal_Zprime_mutau4000_nomWeight"
-# "output_nominal_Zprime_mutau4500_nomWeight"
-# "output_nominal_Zprime_mutau500_nomWeight"
-# "output_nominal_Zprime_mutau5000_nomWeight"
-# "output_nominal_Zprime_mutau5500_nomWeight"
-# "output_nominal_Zprime_mutau6000_nomWeight"
-# "output_nominal_Zprime_mutau6500_nomWeight"
-# "output_nominal_Zprime_mutau700_nomWeight"
-# "output_nominal_Zprime_mutau7000_nomWeight"
-# "output_nominal_Zprime_mutau7500_nomWeight"
-# "output_nominal_Zprime_mutau8000_nomWeight"
