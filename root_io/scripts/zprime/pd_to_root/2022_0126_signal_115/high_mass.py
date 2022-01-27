59616940513b2faeb4546897a1f932e32e650165
Zhe Yang <zhe.yang@cern.ch>
2022-01-27 17:42:51 +0100

    Update configs for limit check 22-0126

---
+++ root_io/scripts/zprime/pd_to_root/2022_0126_signal_115/high_mass.py
@@ -0,0 +1,107 @@
+import array
+import pathlib
+
+import numpy as np
+import pandas as pd
+import ROOT
+
+df_dir = pathlib.Path("/data/zprime/data_frames_fit/22-0126-sys/high_mass")
+ntup_dir = pathlib.Path("/data/zprime/ntuples_fit/22-0126-sys/high_mass")
+ntup_dir.mkdir(parents=True, exist_ok=True)
+
+samples = [
+    "bkg_qcd",
+    # "bkg_ggZZ",
+    "sig_Zp042",
+    "sig_Zp045",
+    "sig_Zp048",
+    "sig_Zp051",
+    "sig_Zp054",
+    "sig_Zp057",
+    "sig_Zp060",
+    "sig_Zp063",
+    "sig_Zp066",
+    "sig_Zp069",
+    "sig_Zp072",
+    "sig_Zp075",
+]
+branch_list = ["mz1", "mz2", "dnn_out_sig", "weight"]
+sys_wt_list = [
+    "weight_qcd_scale_up_mz1",
+    "weight_qcd_scale_down_mz1",
+    "weight_qcd_scale_up_mz2",
+    "weight_qcd_scale_down_mz2",
+    "weight_pdf_up_mz1",
+    "weight_pdf_up_mz2",
+    "weight_alpha_s_up_mz1",
+    "weight_alpha_s_up_mz2",
+]
+ntup_name = "ntup"
+
+variations = [
+    "tree_NOMINAL",
+    "tree_FT_EFF_B_systematics__1down",
+    "tree_FT_EFF_B_systematics__1up",
+    "tree_FT_EFF_C_systematics__1down",
+    "tree_FT_EFF_C_systematics__1up",
+    "tree_FT_EFF_Light_systematics__1down",
+    "tree_FT_EFF_Light_systematics__1up",
+    "tree_FT_EFF_extrapolation__1down",
+    "tree_FT_EFF_extrapolation__1up",
+    "tree_FT_EFF_extrapolation_from_charm__1down",
+    "tree_FT_EFF_extrapolation_from_charm__1up",
+    "tree_MUON_EFF_ISO_STAT__1down",
+    "tree_MUON_EFF_ISO_STAT__1up",
+    "tree_MUON_EFF_ISO_SYS__1down",
+    "tree_MUON_EFF_ISO_SYS__1up",
+    "tree_MUON_EFF_RECO_STAT__1down",
+    "tree_MUON_EFF_RECO_STAT__1up",
+    "tree_MUON_EFF_RECO_STAT_LOWPT__1down",
+    "tree_MUON_EFF_RECO_STAT_LOWPT__1up",
+    "tree_MUON_EFF_RECO_SYS__1down",
+    "tree_MUON_EFF_RECO_SYS__1up",
+    "tree_MUON_EFF_RECO_SYS_LOWPT__1down",
+    "tree_MUON_EFF_RECO_SYS_LOWPT__1up",
+    "tree_MUON_EFF_TTVA_STAT__1down",
+    "tree_MUON_EFF_TTVA_STAT__1up",
+    "tree_MUON_EFF_TTVA_SYS__1down",
+    "tree_MUON_EFF_TTVA_SYS__1up",
+    "tree_MUON_ID__1down",
+    "tree_MUON_ID__1up",
+    "tree_MUON_MS__1down",
+    "tree_MUON_MS__1up",
+    "tree_MUON_SAGITTA_RESBIAS__1down",
+    "tree_MUON_SAGITTA_RESBIAS__1up",
+    "tree_MUON_SAGITTA_RHO__1down",
+    "tree_MUON_SAGITTA_RHO__1up",
+    "tree_MUON_SCALE__1down",
+    "tree_MUON_SCALE__1up",
+    "tree_PRW_DATASF__1down",
+    "tree_PRW_DATASF__1up",
+]
+
+for var in variations:
+    print(f"Variation: {var}")
+    ntup_dir_var = ntup_dir / var
+    ntup_dir_var.mkdir(parents=True, exist_ok=True)
+    for sample in samples:
+        print(f"Generating fit ntuples for: {sample}")
+        # read dataframe
+        df_path = df_dir / var / f"{sample}.feather"
+        df: pd.DataFrame = pd.read_feather(df_path)
+        # prepare ntuple file
+        ntup_path = ntup_dir_var / f"{sample}.root"
+        root_file = ROOT.TFile(ntup_path.as_posix(), "RECREATE")
+        root_file.cd()
+        # dump
+        dump_features = branch_list[:]
+        if sample == "bkg_qcd" and var == "tree_NOMINAL":
+            dump_features += sys_wt_list
+        ntup = ROOT.TNtuple(ntup_name, ntup_name, ":".join(dump_features))
+        fit_array = df[dump_features].to_numpy()
+        for row in fit_array:
+            fill_values = list(row)
+            ntup.Fill(array.array("f", fill_values))
+        root_file.cd()
+        ntup.Write()
+        root_file.Close()
---
+++ share/zprime/master/default/low_mass/dump_fit_inputs_signal_115/prepare_configs_sys.py
@@ -0,0 +1,100 @@
+basic = """config:
+    include:
+        - "share/zprime/master/default/low_mass/train.yaml"
+
+job:
+    job_name: "dump_ntup_sys_{p_variation}"
+    job_type: "apply"
+    train_job_name: "default_train"
+
+input:
+    input_path: "zprime/data_frames/22-0126-sys/low_mass/{p_variation}/zprime_low_m_quadtype_2_new_mc.feather"
+    negative_weight_process: "keep"
+    reset_feature: false
+
+    bkg_list:
+        - "bkg_qcd"
+    data_list: []
+
+"""
+apply = """
+apply:
+    book_fit_inputs: true
+    fit_df:
+        branches:
+            - "mz1"
+            - "mz2"
+        save_dir: "zprime/data_frames_fit/22-0126-sys/low_mass/{p_variation}"
+
+"""
+apply_nominal = """
+apply:
+    book_fit_inputs: true
+    fit_df:
+        branches:
+            - "mz1"
+            - "mz2"
+            - "weight_qcd_scale_up_mz1"
+            - "weight_qcd_scale_down_mz1"
+            - "weight_qcd_scale_up_mz2"
+            - "weight_qcd_scale_down_mz2"
+            - "weight_pdf_up_mz1"
+            - "weight_pdf_up_mz2"
+            - "weight_alpha_s_up_mz1"
+            - "weight_alpha_s_up_mz2"
+        save_dir: "zprime/data_frames_fit/22-0126-sys/low_mass/{p_variation}"
+
+"""
+
+temp = basic + apply
+temp_nominal = basic + apply_nominal
+
+
+variations = [
+    "tree_NOMINAL",
+    "tree_FT_EFF_B_systematics__1down",
+    "tree_FT_EFF_B_systematics__1up",
+    "tree_FT_EFF_C_systematics__1down",
+    "tree_FT_EFF_C_systematics__1up",
+    "tree_FT_EFF_Light_systematics__1down",
+    "tree_FT_EFF_Light_systematics__1up",
+    "tree_FT_EFF_extrapolation__1down",
+    "tree_FT_EFF_extrapolation__1up",
+    "tree_FT_EFF_extrapolation_from_charm__1down",
+    "tree_FT_EFF_extrapolation_from_charm__1up",
+    "tree_MUON_EFF_ISO_STAT__1down",
+    "tree_MUON_EFF_ISO_STAT__1up",
+    "tree_MUON_EFF_ISO_SYS__1down",
+    "tree_MUON_EFF_ISO_SYS__1up",
+    "tree_MUON_EFF_RECO_STAT__1down",
+    "tree_MUON_EFF_RECO_STAT__1up",
+    "tree_MUON_EFF_RECO_STAT_LOWPT__1down",
+    "tree_MUON_EFF_RECO_STAT_LOWPT__1up",
+    "tree_MUON_EFF_RECO_SYS__1down",
+    "tree_MUON_EFF_RECO_SYS__1up",
+    "tree_MUON_EFF_RECO_SYS_LOWPT__1down",
+    "tree_MUON_EFF_RECO_SYS_LOWPT__1up",
+    "tree_MUON_EFF_TTVA_STAT__1down",
+    "tree_MUON_EFF_TTVA_STAT__1up",
+    "tree_MUON_EFF_TTVA_SYS__1down",
+    "tree_MUON_EFF_TTVA_SYS__1up",
+    "tree_MUON_ID__1down",
+    "tree_MUON_ID__1up",
+    "tree_MUON_MS__1down",
+    "tree_MUON_MS__1up",
+    "tree_MUON_SAGITTA_RESBIAS__1down",
+    "tree_MUON_SAGITTA_RESBIAS__1up",
+    "tree_MUON_SAGITTA_RHO__1down",
+    "tree_MUON_SAGITTA_RHO__1up",
+    "tree_MUON_SCALE__1down",
+    "tree_MUON_SCALE__1up",
+    "tree_PRW_DATASF__1down",
+    "tree_PRW_DATASF__1up",
+]
+
+for variation in variations:
+    with open(f"sys_ntup_{variation}.yaml", "w+") as file:
+        if variation == "tree_NOMINAL":
+            file.write(temp_nominal.format(p_variation=variation))
+        else:
+            file.write(temp.format(p_variation=variation))
